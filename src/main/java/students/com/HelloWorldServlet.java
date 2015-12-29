c1f4623a2c5ea6aa14d6f72717ff24007b4cecf5
Yuriy Pylypenko <yupylypenko@ingo.ua>
2015-12-29 12:17:02 +0200

    First commit

---
+++ src/main/java/students/com/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package students.web;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class HelloWorldServlet extends HttpServlet {
+ 
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+ 
+        PrintWriter pw = resp.getWriter();
+        pw.println("<H1>Hello, world! или Привет мир! Это я!</H1>");
+        pw.println("<H1>Следующая моя стpаница нов!</H1>");
+ 
+    }
+}
