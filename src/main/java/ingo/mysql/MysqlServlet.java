99ee8f283ad2716d56be12da2ec496ebb3816bae
Yuriy Pylypenko <yupylypenko@ingo.ua>
2016-01-05 16:31:43 +0200

    Add username GET

--- src/main/java/ingo/mysql/MysqlServlet.java
+++ src/main/java/ingo/mysql/MysqlServlet.java
@@ -12,22 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 public class MysqlServlet extends HttpServlet {
  
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String username = (String) req.getParameter("username");
+    	username = username.toUpperCase();
+    	resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println("<!DOCTYPE HTML>");
+                      
+        PrintWriter pw = resp.getWriter();
+        pw.println("<title>Мой первый сервлет!</title>");
+        pw.println("<H1>Следующая моя стpаница GET!</H1>"); 
+        pw.println("<html><body>");
+        pw.println("<p>"+username+"</p>");
+                
+    	pw.print("<form method=\"GET\">");
+    	pw.println("<p> Введите имя пользователя: <input type=\"text\" name=\"username\"/></p>");
+    	pw.println("<input type=\"submit\" value=\"Отправить!\"/></form>");
+    	       
+        pw.println("</body></html>");
+ 
+    }
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
         resp.getWriter().println("<!DOCTYPE HTML>");
                       
         PrintWriter pw = resp.getWriter();
-        pw.println("<H1>Hello, world! или Привет мир! Это я!</H1>");
-        pw.println("<H1>Следующая моя стpаница!</H1>"); 
+        pw.println("<title>Мой первый сервлет!</title>");
+        pw.println("<H1>Следующая моя стpаница POST!</H1>"); 
         pw.println("<html><body>");
-        pw.println("<p> Yurec </p>");
         
-    	pw.print("<form method=\"GET\">");
-    	pw.println("<p> Введите имя пользователя: <input type=\"text\" name=\"username\"/></p>");
-    	pw.println("<input type=\"submit\" value=\"Отправить!\"/></form>");
-    	
-        pw.print("<form method=\"post\">");
-        pw.println("<input type=\"text\" name=\"param1\"/>");
-        pw.println("<input type=\"submit\" value=\"CLICK HERE\"/></form>");
+    	pw.println("<p> Введите имя пользователя: </p>");
+    	    	       
         pw.println("</body></html>");
  
     }
