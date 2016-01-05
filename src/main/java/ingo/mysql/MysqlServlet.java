c3626fc76669a939e48e5230d762710cc01d467d
Yuriy Pylypenko <yupylypenko@ingo.ua>
2016-01-05 15:29:54 +0200

    Hello world!

--- src/main/java/ingo/mysql/MysqlServlet.java
+++ src/main/java/ingo/mysql/MysqlServlet.java
@@ -1,17 +1,16 @@
 package ingo.mysql;
+ 
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+ 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-public class MysqlServlet {
-    public String concat(String s1, String s2) {
-	return s1 + s2;	
-	
-    }
+ 
+public class MysqlServlet extends HttpServlet {
+ 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
  
@@ -20,4 +19,8 @@ public class MysqlServlet {
         pw.println("<H1>Следующая моя стpаница 1!</H1>");
  
     }
+    public String concat(String s1, String s2) {
+    	return s1 + s2;	
+    	
+        }
 }
